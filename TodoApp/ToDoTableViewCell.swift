d68f1133052ff13f50989004ac652f4d13e78893
davalcato <davalcato@yahoo.com>
2018-02-14 19:59:37 -0800

    first commit

---
+++ TodoApp/ToDoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ToDoTableViewCell.swift
+//  TodoApp
+//
+//  Created by Daval Cato on 2/14/18.
+//  Copyright © 2018 Daval Cato. All rights reserved.
+//
+
+import UIKit
+
+class ToDoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
